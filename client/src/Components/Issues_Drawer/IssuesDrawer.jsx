6b26ab4d54c307a79f53df00de42c3a8d6c31014
Melody Ruth <51979566+Melody-Ruth@users.noreply.github.com>
2021-02-20 14:18:36 -0800

    Merging changes (#34)

--- client/src/Components/Issues_Drawer/IssuesDrawer.jsx
+++ client/src/Components/Issues_Drawer/IssuesDrawer.jsx
@@ -6,9 +6,16 @@ import Divider from '@material-ui/core/Divider';
 import rectLogo from '../../graphics/logo_rect.png';
 import DrawerElem from './DrawerElem';
 import '../../App.css';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import { useEffect } from 'react';
 import axios from "axios";
 import './issuedrawer.css';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 const useStyles = makeStyles((theme) => ({
   drawerPaper: {
@@ -27,7 +34,13 @@ export default function IssuesDrawer() {
       loadEvents();
     },
     []);
-    const [issuesData, setIssuesData] = useState(null)
+    const [issuesData, setIssuesData] = useState(null);
+    const [filter, setFilter] = useState({
+        type: "Vaccination Centers"
+    })
+    const updateFilter = (e) => {
+      setFilter({...filter, type: e.target.value});
+    }
     return (
         <Drawer
         className="IssuesDrawer"
@@ -44,13 +57,33 @@ export default function IssuesDrawer() {
             style={{width:"80%"}}
         />
         </div>
+        <ListItem>
+          <ListItemText>Filter by type </ListItemText>
+          <FormControl className={classes.formControl}>
+            <Select
+              value={filter.type}
+              onChange={updateFilter}
+              displayEmpty
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value="All">All</MenuItem>
+              <MenuItem value="Natural Disasters">Natural Disasters</MenuItem>
+              <MenuItem value="Public Disturbances">Public Disturbances</MenuItem>
+              <MenuItem value="Vaccination Centers">Vaccination Centers</MenuItem>
+              <MenuItem value="Food Services">Food Services</MenuItem>
+            </Select>
+          </FormControl>
+        </ListItem>
         <Divider />
         <List>
-        {issuesData != null ? issuesData.map(item => (
+        {issuesData != null ? issuesData.map(item => ( filter.type == "All" || filter.type == item.type ?
           <div>
           <DrawerElem title={item.title} likes={item.upvotes} type={item.type}/>
           <hr class="dividerColor" />
           </div>
+          : <div></div>
         )) : <div></div>}
         </List>
       </Drawer>
