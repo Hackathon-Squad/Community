57b670d5cc72c107a8c4c90221ede68f05e80262
Advay Sharma <sharmaadvay0@gmail.com>
2021-02-20 15:47:18 -0800

    fixed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Map from './Components/Map/Map';
-import './App.css';
-import IssuesDrawer from './Components/Issues_Drawer/IssuesDrawer';
-import { BrowserRouter, Route } from 'react-router-dom';
-import ReportButton from './Components/Report_Issue/ReportButton';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import React from "react";
+import Map from "./Components/Map/Map";
+import "./App.css";
+import IssuesDrawer from "./Components/Issues_Drawer/IssuesDrawer";
+import { BrowserRouter, Route } from "react-router-dom";
+import ReportButton from "./Components/Report_Issue/ReportButton";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <div className="root">
-      <CssBaseline/>
-      <IssuesDrawer/>
-      <Map/>
-      <ReportButton />
-    </div>
+      <div className="root">
+        <CssBaseline />
+        <IssuesDrawer />
+        <Map />
+        <ReportButton />
+      </div>
     </BrowserRouter>
   );
 }
