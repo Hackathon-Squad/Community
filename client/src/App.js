75e6ed2196515ac3a9b343463c7d2cc31914078a
Melody-Ruth <melody@sd-fun.org>
2021-02-19 23:34:44 -0800

    rebasing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
-import "./App.css";
-import Map from "./Components/Map/Map.jsx";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Map from './Components/Map/Map.jsx';
+import Form from './Components/Report_Issue/Form.js';
+import './App.css';
+import IssuesDrawer from './Components/Issues_Drawer/IssuesDrawer.js';
 
-const App = () => {
+export default function App() {
   return (
-    <div>
-      <Map />
+    <div className="root">
+      <IssuesDrawer/>
+      <Map/>
+      <Form/>
     </div>
   );
-};
-
-export default App;
+}
