b3d4fbc03480cef00444d6379dd0e277102880c1
Nishant Balaji <44332326+nishantbalaji@users.noreply.github.com>
2021-02-20 15:44:54 -0800

    Map (#36)
    * map markers
    
    * clickable markers

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Map from './Components/Map/Map';
-import Form from './Components/Report_Issue/Form';
-import Form2 from './Components/Report_Issue/Form2';
 import './App.css';
 import IssuesDrawer from './Components/Issues_Drawer/IssuesDrawer';
-import NewReportButton from './Components/Report_Issue/NewIssueButton';
 import { BrowserRouter, Route } from 'react-router-dom';
 import ReportButton from './Components/Report_Issue/ReportButton';
 import CssBaseline from '@material-ui/core/CssBaseline';
--- client/src/Components/Map/Map.jsx
+++ client/src/Components/Map/Map.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
-import ReactMapGL from "react-map-gl";
-
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import ReactMapGL, { Marker, FlyToInterpolator } from "react-map-gl";
+import axios from 'axios';
+import img from '../../graphics/redpin.svg';
+import * as d3 from "d3";
 
 const Map = () => {
   const [viewport, setViewport] = useState({
@@ -11,13 +14,52 @@ const Map = () => {
     zoom: 12,
   });
 
+  useEffect(() => loadEvents(), [])
+
+  const [markers, setMarkers] = useState(null)
+
+  const loadEvents = async () => {
+    const result = await axios.get("/api/posts");
+    console.log(result.data);
+    setMarkers(result.data);
+  }
+
+  const handleClick = (e) => {
+    setViewport({
+      ...viewport,
+      longitude: e.longitude,
+      latitude: e.latitude,
+      zoom: 16,
+      transitionDuration: 2000,
+      transitionInterpolator: new FlyToInterpolator(),
+      transitionEasing: d3.easeCubicInOut
+    });
+  }
+
   return (
     <ReactMapGL
       {...viewport}
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
       mapStyle="mapbox://styles/mapbox/streets-v11"
       onViewportChange={(nextViewport) => setViewport(nextViewport)}
-    />
+    >
+
+     {markers ? 
+     ( 
+      markers.map(
+        city => (
+          
+        <Marker longitude={city.coordinates.longitude} latitude={city.coordinates.latitude} onClick={() => handleClick(city.coordinates)}>
+          <img src={img} />
+        </Marker>
+        
+          )
+        )
+      ) 
+      : null}
+
+      
+      </ReactMapGL>
   );
 };
 
