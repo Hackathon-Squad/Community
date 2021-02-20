f5fab908e70196bd2aaf10adbcbb00fb23a89c89
Advay Sharma <sharmaadvay0@gmail.com>
2021-02-19 22:56:06 -0800

    added get posts route

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../db");
 
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+  const collection = db.collection("posts");
+  const documents = await collection.get();
+  let docArray = [];
+  try {
+    documents.forEach((doc) => docArray.push(doc.data()));
+  } catch (error) {
+    console.error(error);
+  }
+  res.status(200).send(docArray);
+});
 
 module.exports = router;
