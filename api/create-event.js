f0e288cd9df5da001ba90d89575fade8de18e110
Advay Sharma <52980112+sharmaadvay0@users.noreply.github.com>
2021-02-19 22:59:22 -0800

    added get posts route (#23)

--- api/create-event.js
+++ api/create-event.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
   const doc = db.collection("posts").doc();
   try {
     await doc.set(req.body);
