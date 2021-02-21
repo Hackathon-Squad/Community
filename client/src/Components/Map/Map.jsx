5ba52cc5e797caf813715f119b6e8717a099544b
Advay Sharma <52980112+sharmaadvay0@users.noreply.github.com>
2021-02-20 18:04:56 -0800

    Marker popup (#40)
    * basic popup with info
    
    * cleaned up popup

--- client/src/Components/Map/Map.jsx
+++ client/src/Components/Map/Map.jsx
@@ -1,9 +1,10 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import ReactMapGL, { Marker, FlyToInterpolator } from "react-map-gl";
-import axios from 'axios';
-import img from '../../graphics/redpin.svg';
+import * as React from "react";
+import { useState, useEffect } from "react";
+import ReactMapGL, { Marker, FlyToInterpolator, Popup } from "react-map-gl";
+import axios from "axios";
+import img from "../../graphics/redpin.svg";
 import * as d3 from "d3";
+import "../../App.css";
 
 const Map = () => {
   const [viewport, setViewport] = useState({
@@ -14,27 +15,42 @@ const Map = () => {
     zoom: 12,
   });
 
-  useEffect(() => loadEvents(), [])
+  useEffect(() => loadEvents(), []);
 
-  const [markers, setMarkers] = useState(null)
+  const [markers, setMarkers] = useState(null);
+  const [displayPopup, setDisplayPopup] = useState({
+    display: false,
+    latitude: 32.8801,
+    longitude: -117.234,
+    imageURL: "",
+    title: "",
+    description: "",
+  });
 
   const loadEvents = async () => {
     const result = await axios.get("/api/posts");
-    console.log(result.data);
     setMarkers(result.data);
-  }
+  };
 
   const handleClick = (e) => {
     setViewport({
       ...viewport,
-      longitude: e.longitude,
-      latitude: e.latitude,
+      longitude: e.coordinates.longitude,
+      latitude: e.coordinates.latitude,
       zoom: 16,
       transitionDuration: 2000,
       transitionInterpolator: new FlyToInterpolator(),
-      transitionEasing: d3.easeCubicInOut
+      transitionEasing: d3.easeCubicInOut,
     });
-  }
+    setDisplayPopup({
+      display: true,
+      latitude: e.coordinates.latitude,
+      longitude: e.coordinates.longitude,
+      imageURL: e.imageURL,
+      title: e.title,
+      description: e.description,
+    });
+  };
 
   return (
     <ReactMapGL
@@ -42,24 +58,52 @@ const Map = () => {
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
       mapStyle="mapbox://styles/mapbox/streets-v11"
       onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      onMouseDown={() => {
+        if (displayPopup.display) {
+          setDisplayPopup({ ...displayPopup, display: false });
+        }
+      }}
     >
-
-     {markers ? 
-     ( 
-      markers.map(
-        city => (
-          
-        <Marker longitude={city.coordinates.longitude} latitude={city.coordinates.latitude} onClick={() => handleClick(city.coordinates)}>
-          <img src={img} />
-        </Marker>
-        
-          )
-        )
-      ) 
-      : null}
-
-      
-      </ReactMapGL>
+      {markers &&
+        markers.map((city) => (
+          <Marker
+            longitude={city.coordinates.longitude}
+            latitude={city.coordinates.latitude}
+            onClick={() => handleClick(city)}
+          >
+            <img src={img} className="pin" />
+          </Marker>
+        ))}
+      {displayPopup.display && (
+        <Popup
+          latitude={displayPopup.latitude}
+          longitude={displayPopup.longitude}
+          closeButton
+          closeOnClick
+          onClose={() =>
+            setDisplayPopup({
+              display: false,
+              latitude: 32.8801,
+              longitude: -117.234,
+              imageURL: "",
+              title: "",
+              description: "",
+            })
+          }
+          anchor="bottom"
+          offsetLeft={10}
+          tipSize={10}
+        >
+          <div>{displayPopup.title}</div>
+          <div>{displayPopup.description}</div>
+          <img
+            src={displayPopup.imageURL}
+            style={{ objectFit: "contain", width: 300 }}
+            className="pin"
+          />
+        </Popup>
+      )}
+    </ReactMapGL>
   );
 };
 
--- client/src/Components/Map/Markers.jsx
+++ client/src/Components/Map/Markers.jsx
@@ -1,44 +1,28 @@
-import { Marker } from 'react-map-gl';
-import axios from 'axios';
-import React, { useState } from 'react';
-
+import { Marker } from "react-map-gl";
+import axios from "axios";
+import React, { useState } from "react";
+import img from "../../graphics/redpin.svg";
+import "../../App.css";
 
 const MarkerItem = () => {
+  const [drag, setDrag] = useState({
+    lat: 32.8801,
+    lon: -117.234,
+    move: true,
+  });
 
-    const [drag, setDrag] = useState({
-        lat: 32.8801,
-        lon: -117.234,
-        move: true
-      });
+  return (
+    <Marker
+      latitude={drag.lat}
+      longitude={drag.lon}
+      onDrag={(event) =>
+        setDrag({ ...drag, lat: event.lngLat[1], lon: event.lngLat[0] })
+      }
+      draggable={drag.move}
+    >
+      <img src={img} className="pin" />
+    </Marker>
+  );
+};
 
-        const handleDragEnd = async (event) => {
-          const lng = event.lngLat[0];
-          const lat = event.lngLat[1];
-
-          const head = { 
-            headers: {
-            "Content-Type": "application/json",
-            }
-          };
-
-          const body = {
-            "longitude" : lng,
-            "latitude" : lat,
-          };
-
-          const result = await axios.post("/api/create-event", body, head)
-          console.log(result);
-          setDrag({...drag, move:false, lat:lat, lon:lng})
-        };
-
-    return (
-        <Marker latitude={drag.lat} longitude={drag.lon} 
-          onDragEnd={(event) => handleDragEnd(event)} 
-          onDrag={(event) => setDrag({...drag, lat:event.lngLat[1], lon:event.lngLat[0]})}  
-          draggable={drag.move}>
-             <img src="../../redpin.svg"/>
-        </Marker>
-    )
-}
-
-export default MarkerItem; 
+export default MarkerItem;
