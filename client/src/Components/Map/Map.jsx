fea43eed31915581bf383c80f1e5aca265c67473
Advay Sharma <sharmaadvay0@gmail.com>
2021-02-20 17:13:31 -0800

    basic popup with info

--- client/src/Components/Map/Map.jsx
+++ client/src/Components/Map/Map.jsx
@@ -1,9 +1,10 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import ReactMapGL, { Marker, FlyToInterpolator } from "react-map-gl";
-import axios from 'axios';
-import img from '../../graphics/redpin.svg';
+import * as React from "react";
+import { useState, useEffect } from "react";
+import ReactMapGL, { Marker, FlyToInterpolator, Popup } from "react-map-gl";
+import axios from "axios";
+import img from "../../graphics/redpin.svg";
 import * as d3 from "d3";
+import "../../App.css";
 
 const Map = () => {
   const [viewport, setViewport] = useState({
@@ -14,27 +15,43 @@ const Map = () => {
     zoom: 12,
   });
 
-  useEffect(() => loadEvents(), [])
+  useEffect(() => loadEvents(), []);
 
-  const [markers, setMarkers] = useState(null)
+  const [markers, setMarkers] = useState(null);
+  const [displayPopup, setDisplayPopup] = useState({
+    display: false,
+    latitude: 32.8801,
+    longitude: -117.234,
+    imageURL: "",
+    title: "",
+    description: "",
+  });
 
   const loadEvents = async () => {
     const result = await axios.get("/api/posts");
-    console.log(result.data);
     setMarkers(result.data);
-  }
+  };
 
   const handleClick = (e) => {
+    setDisplayPopup(true);
+    setDisplayPopup({
+      display: true,
+      latitude: e.coordinates.latitude,
+      longitude: e.coordinates.longitude,
+      imageURL: e.imageURL,
+      title: e.title,
+      description: e.description,
+    });
     setViewport({
       ...viewport,
-      longitude: e.longitude,
-      latitude: e.latitude,
+      longitude: e.coordinates.longitude,
+      latitude: e.coordinates.latitude,
       zoom: 16,
       transitionDuration: 2000,
       transitionInterpolator: new FlyToInterpolator(),
-      transitionEasing: d3.easeCubicInOut
+      transitionEasing: d3.easeCubicInOut,
     });
-  }
+  };
 
   return (
     <ReactMapGL
@@ -43,23 +60,46 @@ const Map = () => {
       mapStyle="mapbox://styles/mapbox/streets-v11"
       onViewportChange={(nextViewport) => setViewport(nextViewport)}
     >
-
-     {markers ? 
-     ( 
-      markers.map(
-        city => (
-          
-        <Marker longitude={city.coordinates.longitude} latitude={city.coordinates.latitude} onClick={() => handleClick(city.coordinates)}>
-          <img src={img} />
-        </Marker>
-        
-          )
-        )
-      ) 
-      : null}
-
-      
-      </ReactMapGL>
+      {markers &&
+        markers.map((city) => (
+          <Marker
+            longitude={city.coordinates.longitude}
+            latitude={city.coordinates.latitude}
+            onClick={() => handleClick(city)}
+          >
+            <img src={img} />
+          </Marker>
+        ))}
+      {displayPopup.display && (
+        <Popup
+          latitude={displayPopup.latitude}
+          longitude={displayPopup.longitude}
+          closeButton
+          closeOnClick
+          onClose={() =>
+            setDisplayPopup({
+              display: false,
+              latitude: 32.8801,
+              longitude: -117.234,
+              imageURL: "",
+              title: "",
+              description: "",
+            })
+          }
+          anchor="bottom"
+          offsetLeft={10}
+          tipSize={10}
+        >
+          <div>{displayPopup.title}</div>
+          <div>{displayPopup.description}</div>
+          <img
+            src="https://storage.googleapis.com/community-post-images/2a911e90-3580-4bd4-b050-25fd5f53d247"
+            style={{ objectFit: "contain", width: 500 }}
+            className="pin"
+          />
+        </Popup>
+      )}
+    </ReactMapGL>
   );
 };
 
