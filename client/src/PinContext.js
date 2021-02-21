ff5a49e5fa10528fb9c7078d9b03e6a86d0fda33
Advay Sharma <sharmaadvay0@gmail.com>
2021-02-20 22:03:31 -0800

    added clicking animation to map

--- client/src/PinContext.js
+++ client/src/PinContext.js
@@ -1,29 +1,29 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState } from "react";
 
 const PinContext = React.createContext();
 const PinUpdateContext = React.createContext();
 
 export function usePin() {
-    return useContext(PinContext);
+  return useContext(PinContext);
 }
 
 export function usePinUpdate() {
-    return useContext(PinUpdateContext);
+  return useContext(PinUpdateContext);
 }
 
 export function PinProvider({ children }) {
-    const [pinnedEvent, setPinnedEvent] = useState(0);//Should be a uuid
-    const [testNum, setTestNum] = useState(0);//Should be a uuid
+  const [pinnedEvent, setPinnedEvent] = useState(0); //Should be a uuid
+  const [testNum, setTestNum] = useState(0); //Should be a uuid
 
-    function toggleNum() {
-        setTestNum(prevNum => prevNum++);
-    }
+  function toggleNum() {
+    setTestNum((prevNum) => prevNum++);
+  }
 
-    return (
-        <PinContext.Provider value={pinnedEvent}>
-            <PinUpdateContext.Provider value={setPinnedEvent}>
-                {children}
-            </PinUpdateContext.Provider>
-        </PinContext.Provider>
-    )
+  return (
+    <PinContext.Provider value={pinnedEvent}>
+      <PinUpdateContext.Provider value={setPinnedEvent}>
+        {children}
+      </PinUpdateContext.Provider>
+    </PinContext.Provider>
+  );
 }
