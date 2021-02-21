e8daab1192db3b7b48463f15998197d7d6f8be7e
Advay Sharma <sharmaadvay0@gmail.com>
2021-02-20 22:21:30 -0800

    fixed submit hide

--- client/src/Components/Report_Issue/Form.jsx
+++ client/src/Components/Report_Issue/Form.jsx
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import Card from '@material-ui/core/Card';
-import { Typography } from '@material-ui/core';
+import React, { useState, useEffect } from "react";
+import Card from "@material-ui/core/Card";
+import { Typography } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Box from "@material-ui/core/Box";
-import { ThemeProvider } from '@material-ui/core/styles';
-import myTheme from './MyTheme';
-import {useHistory} from 'react-router-dom';
+import { ThemeProvider } from "@material-ui/core/styles";
+import myTheme from "./MyTheme";
+import { useHistory } from "react-router-dom";
 import ReactMapGL from "react-map-gl";
-import './form.css';
-import MarkerItem from '../Map/Markers';
-import axios from 'axios';
+import "./form.css";
+import MarkerItem from "../Map/Markers";
+import axios from "axios";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
-const Form = (props) => {
+const Form = ({ show, setShow }) => {
   const [uploadedFile, setUploadedFile] = useState(false);
   const [fileName, setFileName] = useState("");
   const history = useHistory();
@@ -46,15 +46,14 @@ const Form = (props) => {
     imgURL,
   } = formData;
 
-  const [hide, setHide ] = useState(true);
+  const [hide, setHide] = useState(true);
   const onButton = () => setHide(!hide);
 
-  const { temp, setTemp } = props;
-
-  useEffect(() =>
-  setTemp(false),
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  [temp])
+  // useEffect(
+  //   () => setTemp(false),
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  //   [temp]
+  // );
 
   const uploadFile = async (fileData) => {
     console.log(title);
@@ -67,14 +66,14 @@ const Form = (props) => {
     const formData2 = new FormData();
     formData2.append("image", fileData);
     const result = await axios.post("/api/upload", formData2, config);
-    setFileName("Uploaded: "+fileData.name);
+    setFileName("Uploaded: " + fileData.name);
     console.log(title);
-    setFormData({...formData, imgURL: result.data.url});
+    setFormData({ ...formData, imgURL: result.data.url });
     //url for image is in result.data
     //need to add it to the object when uploading post to the db
     console.log(title);
   };
- 
+
   const titleTried = () => {
     setFormData({ ...formData, triedTitle: true });
   };
@@ -108,10 +107,13 @@ const Form = (props) => {
     if (checkTitle() && checkDescription()) {
       console.log("Cleared to submit");
       const newItem = {};
-      newItem.id =  uuidv4();
+      newItem.id = uuidv4();
       newItem.title = title;
       newItem.description = description;
-      newItem.coordinates = { latitude: viewport.latitude, longitude: viewport.longitude };
+      newItem.coordinates = {
+        latitude: viewport.latitude,
+        longitude: viewport.longitude,
+      };
       newItem.type = type;
       newItem.imageURL = imgURL;
       newItem.upvotes = 0;
@@ -123,125 +125,128 @@ const Form = (props) => {
     } else {
       setFormData({ ...formData, triedTitle: true, triedDescription: true });
     }
-    onButton();
+    setShow(false);
     console.log("hi:");
     console.log(hide);
+  };
 
-  }
-
-    return (
-        <div className="Modal">
-            <Card id="reportCard" className="Form">
-                <Typography variant="h4" style={{fontFamily: 'Patua One', marginBottom:15}}>
-                    New Report:
-                </Typography>
-                <form noValidate autoComplete="off" onSubmit={onSubmit}>
-              <div id="form-inputs">
-                <Box pb={1.87} width="100%">
-                  <TextField
-                    id="title"
-                    value={title}
-                    required
-                    label="Title"
-                    variant="outlined"
-                    onChange={(e) => onChangeTitle(e)}
-                    error={!checkTitle() && triedTitle}
-                    onBlur={titleTried}
-                    fullWidth={true}
-                  />
-                </Box>
-                <Box pb={1.87} width="100%">
-                  <TextField
-                    id="Description"
-                    required
-                    label="Description"
-                    variant="outlined"
-                    onChange={(e) => onChangeDescription(e)}
-                    onBlur={descriptionTried}
-                    error={!checkDescription() && triedDescription}
-                    fullWidth={true}
-                  />
-                </Box>
-
-              </div>
-    
-            <div className="Map">
-              <ReactMapGL
-                {...viewport}
-                mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-                mapStyle="mapbox://styles/mapbox/streets-v11"
-                onViewportChange={(nextViewport) => setViewport(nextViewport)}
-              > 
-                <MarkerItem/>
-              </ReactMapGL> 
-
-            </div>
-
-            <br/>
-            
-            <div className="Buttons">
-              { uploadedFile ? <Typography variant="h7">{fileName}</Typography> : <ThemeProvider theme={myTheme}>
-              <input
-                id="input"
-                name="image"
-                type="file"
-                hidden
-                onChange={(result) => uploadFile(result.target.files[0])}
+  return (
+    <div className="Modal">
+      <Card id="reportCard" className="Form">
+        <Typography
+          variant="h4"
+          style={{ fontFamily: "Patua One", marginBottom: 15 }}
+        >
+          New Report:
+        </Typography>
+        <form noValidate autoComplete="off" onSubmit={onSubmit}>
+          <div id="form-inputs">
+            <Box pb={1.87} width="100%">
+              <TextField
+                id="title"
+                value={title}
+                required
+                label="Title"
+                variant="outlined"
+                onChange={(e) => onChangeTitle(e)}
+                error={!checkTitle() && triedTitle}
+                onBlur={titleTried}
+                fullWidth={true}
               />
-              <span
-                  onClick={() => global.document.getElementById("input").click()}
+            </Box>
+            <Box pb={1.87} width="100%">
+              <TextField
+                id="Description"
+                required
+                label="Description"
+                variant="outlined"
+                onChange={(e) => onChangeDescription(e)}
+                onBlur={descriptionTried}
+                error={!checkDescription() && triedDescription}
+                fullWidth={true}
+              />
+            </Box>
+          </div>
+
+          <div className="Map">
+            <ReactMapGL
+              {...viewport}
+              mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+              mapStyle="mapbox://styles/mapbox/streets-v11"
+              onViewportChange={(nextViewport) => setViewport(nextViewport)}
+            >
+              <MarkerItem />
+            </ReactMapGL>
+          </div>
+
+          <br />
+
+          <div className="Buttons">
+            {uploadedFile ? (
+              <Typography variant="h7">{fileName}</Typography>
+            ) : (
+              <ThemeProvider theme={myTheme}>
+                <input
+                  id="input"
+                  name="image"
+                  type="file"
+                  hidden
+                  onChange={(result) => uploadFile(result.target.files[0])}
+                />
+                <span
+                  onClick={() =>
+                    global.document.getElementById("input").click()
+                  }
                 >
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    id="upload"
-                  >
+                  <Button variant="contained" color="primary" id="upload">
                     Upload Image
                   </Button>
                 </span>
-                </ThemeProvider> }
-            
-
-                {"     "}
-                
-              <ThemeProvider theme={myTheme}>
-                    <Button
-                      variant="contained"
-                      color="secondary"
-                      type="submit"
-                      id="next"
-               >
-                 Submit
-                </Button>
               </ThemeProvider>
-              </div>
-              { uploadedFile ?
-              <div className="typeInput">
+            )}
+
+            {"     "}
+
+            <ThemeProvider theme={myTheme}>
+              <Button
+                variant="contained"
+                color="secondary"
+                type="submit"
+                id="next"
+              >
+                Submit
+              </Button>
+            </ThemeProvider>
+          </div>
+          {uploadedFile ? (
+            <div className="typeInput">
               <Typography variant="h6">Type: </Typography>
               <FormControl>
-                  <Select
-                    value={formData.type}
-                    onChange={onChangeType}
-                    displayEmpty
-                  >
-                  <MenuItem value="Natural Disasters">Natural Disasters</MenuItem>
-                  <MenuItem value="Public Disturbances">Public Disturbances</MenuItem>
-                  <MenuItem value="Vaccination Centers">Vaccination Centers</MenuItem>
+                <Select
+                  value={formData.type}
+                  onChange={onChangeType}
+                  displayEmpty
+                >
+                  <MenuItem value="Natural Disasters">
+                    Natural Disasters
+                  </MenuItem>
+                  <MenuItem value="Public Disturbances">
+                    Public Disturbances
+                  </MenuItem>
+                  <MenuItem value="Vaccination Centers">
+                    Vaccination Centers
+                  </MenuItem>
                   <MenuItem value="Food Services">Food Services</MenuItem>
                 </Select>
               </FormControl>
-              </div>
-           : 
-           <div></div>}
-          </form>
-            </Card>
-            
-         
-        </div>
-
-
-    );
-}
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </form>
+      </Card>
+    </div>
+  );
+};
 
 export default Form;
-
--- client/src/Components/Report_Issue/Modal.jsx
+++ client/src/Components/Report_Issue/Modal.jsx
@@ -1,33 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Modal from "@material-ui/core/Modal";
-import Form from './Form';
-import './form.css';
+import Form from "./Form";
+import "./form.css";
 
-const CardModal = (props) => {
+const CardModal = ({ show, setShow }) => {
+  const [temp, setTemp] = useState(false);
+  const onButton = () => {
+    setTemp(!temp);
+    console.log("temp : ");
+    console.log(temp);
+  };
 
-    const [temp, setTemp ] = useState(false);
-    const onButton = () => {
-        
-        setTemp(!temp);
-        console.log("temp : ");
-        console.log(temp);
-    }
+  // useEffect(() =>
+  // setShow(false),
+  // // eslint-disable-next-line react-hooks/exhaustive-deps
+  // [temp])
 
-    const { show, setShow } = props;
-
-    useEffect(() =>
-    setShow(false),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [temp])
-
-
-    return(
-        <div>
-            <Modal open={show} onClose={onButton} style={{display:'flex',alignItems:'center',justifyContent:'center'}}>
-              <Form temp={temp} setTemp={setTemp}/>
-            </Modal>
-        </div>
-    );
+  return (
+    <div>
+      <Modal
+        open={show}
+        onClose={onButton}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Form show={show} setShow={setShow} />
+      </Modal>
+    </div>
+  );
 };
 
 export default CardModal;
